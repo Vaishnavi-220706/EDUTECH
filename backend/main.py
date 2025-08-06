f051407b54a34c3c327766ee5e830db89071f4a5
Vaishnavi-220706 <pallaganivaishnavi123@gmail.com>
2025-08-06 11:10:45 +0530

    third update

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-import database,models,auth
+import database,models,auth,chatmodel
 app=FastAPI(title="EdTech API")
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app.include_router(auth.router,prefix="/auth")
+app.include_router(chatmodel.router,prefix="/suggest")
 @app.get("/")
 def home():
     return{"Message":"EdTech API"}
